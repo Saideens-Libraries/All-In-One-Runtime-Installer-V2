db0798b1e5f7df40cac04bf7e0bb2b40aa301a8d
Saideens-Libraries <axjcakysid@rambler.ru>
2026-05-15 21:02:36 -0600

    Updated script

---
+++ Other-Runtimes/Other-Runtime-Installer.bat
@@ -0,0 +1,15 @@
+@echo off
+SETLOCAL ENABLEEXTENSIONS
+
+@echo off
+cd /d %~dp0
+
+echo.
+echo Installing Microsoft XNA Framework Redistributable 4.0....
+echo.
+
+start /wait xnafx40_redist.msi /qr
+echo Successfully installed Microsoft XNA Framework Redistributable 4.0
+
+echo.
+echo All other packages are successfully installed.
